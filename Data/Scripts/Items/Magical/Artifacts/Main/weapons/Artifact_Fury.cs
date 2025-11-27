1e5e096d41d0b9f28347b394539f5350130ff745
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-27 15:50:28 -0300

    fury has a chance of restoring stamina and mana on kill

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_Fury.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_Fury.cs
@@ -15,14 +15,35 @@ namespace Server.Items
 			ItemID = 0x13FF;
             WeaponAttributes.HitFireball = 25;
             WeaponAttributes.HitLightning = 25;
-            WeaponAttributes.SelfRepair = 5;
-            Attributes.CastSpeed = 1;
-            Attributes.ReflectPhysical = 7;
-            Attributes.WeaponSpeed = 16;
+            WeaponAttributes.HitHarm = 25;
+            Attributes.ReflectPhysical = 25;
             Hue = 1357;
 			ArtifactLevel = 2;
-			Server.Misc.Arty.ArtySetup( this, "" );
+			Server.Misc.Arty.ArtySetup( this, "A blade eager of neverending war" );
 		}
+
+        public override void OnHit(Mobile attacker, Mobile defender, double damage)
+        {
+            base.OnHit(attacker, defender, damage);
+
+            if (attacker == null || defender == null)
+                return;
+
+            if (!defender.Alive || defender.Hits <= 0)
+            {
+                if (Utility.Random(100) < 15)
+                {
+                    int stam = Utility.RandomMinMax(5, 25);
+                    int mana = Utility.RandomMinMax(5, 15);
+
+                    attacker.Stam += stam;
+                    attacker.Mana += mana;
+
+                    attacker.SendMessage(33, "Fury empowers you!");
+                }
+            }
+        }
+
 
         public override void GetDamageTypes( Mobile wielder, out int phys, out int fire, out int cold, out int pois, out int nrgy, out int chaos, out int direct )
         {
