b32cfdeeeacb851a38fcac9077ade0d36b4fb285
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-27 18:16:23 -0300

    adds poison on hit to nox bow and crossbow

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_NoxBow.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_NoxBow.cs
@@ -14,17 +14,69 @@ namespace Server.Items
             Name = "Nox Ranger's Light Crossbow";
             Hue = 267;
 			ItemID = 0x13FD;
-            WeaponAttributes.HitLeechHits = 23;
-            WeaponAttributes.HitLeechMana = 22;
-            WeaponAttributes.HitLeechStam = 22;
-            WeaponAttributes.HitLightning = 5;
-            WeaponAttributes.HitLowerAttack = 5;
-            WeaponAttributes.HitPhysicalArea = 5;
+            WeaponAttributes.HitLeechMana = 20;
+            WeaponAttributes.HitLeechStam = 20;
+            WeaponAttributes.HitLowerAttack = 15;
+            WeaponAttributes.HitLowerDefend = 15;
             Attributes.SpellChanneling = 1;
             Attributes.SpellDamage = 10;
             Attributes.WeaponSpeed = 10;
 			ArtifactLevel = 2;
-			Server.Misc.Arty.ArtySetup( this, "" );
+			Server.Misc.Arty.ArtySetup( this, "Holds arrows dripping with venom" );
+		}
+
+        public override void OnHit( Mobile attacker, Mobile defender, double damageBonus )
+		{
+			base.OnHit( attacker, defender, damageBonus );
+
+			if (Utility.RandomDouble() > 0.25)
+				return;
+			double poisoning = attacker.Skills[SkillName.Poisoning].Value;
+		    double roll = Utility.RandomDouble();
+			Poison chosen = null;
+
+    		if (poisoning < 25.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Regular;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 50.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Greater;
+    		    else if (roll < 0.60) chosen = Poison.Regular;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 75.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Deadly;
+    		    else if (roll < 0.40) chosen = Poison.Greater;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 100.0)
+    		{
+    		    if (roll < 0.33) chosen = Poison.Deadly;
+    		    else if (roll < 0.77) chosen = Poison.Greater;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else
+    		{
+    		    if (roll < 0.44) chosen = Poison.Deadly;
+    		    else chosen = Poison.Greater;
+    		}
+
+    		if (chosen != null)
+    		{
+    		    defender.ApplyPoison(attacker, chosen);
+
+    		    if (chosen == Poison.Lesser)
+    		        Misc.Titles.AwardKarma(attacker, -50, true);
+    		    else if (chosen == Poison.Regular)
+    		        Misc.Titles.AwardKarma(attacker, -60, true);
+    		    else if (chosen == Poison.Greater)
+    		        Misc.Titles.AwardKarma(attacker, -70, true);
+    		    else if (chosen == Poison.Deadly)
+    		        Misc.Titles.AwardKarma(attacker, -80, true);
+    		}
 		}
 
         public override void GetDamageTypes( Mobile wielder, out int phys, out int fire, out int cold, out int pois, out int nrgy, out int chaos, out int direct )
--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_NoxRangersHeavyCrossbow.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_NoxRangersHeavyCrossbow.cs
@@ -17,10 +17,63 @@ namespace Server.Items
 			WeaponAttributes.HitLeechStam = 40;
 			Attributes.SpellChanneling = 1;
 			Attributes.WeaponSpeed = 20;
-			Attributes.WeaponDamage = 6;
-			WeaponAttributes.ResistPoisonBonus = 10;
+			WeaponAttributes.ResistPoisonBonus = 20;
 			ArtifactLevel = 2;
-			Server.Misc.Arty.ArtySetup( this, "" );
+			Server.Misc.Arty.ArtySetup( this, "Holds bolts dripping with venom" );
+		}
+
+		public override void OnHit( Mobile attacker, Mobile defender, double damageBonus )
+		{
+			base.OnHit( attacker, defender, damageBonus );
+
+			if (Utility.RandomDouble() > 0.25)
+				return;
+			double poisoning = attacker.Skills[SkillName.Poisoning].Value;
+		    double roll = Utility.RandomDouble();
+			Poison chosen = null;
+
+    		if (poisoning < 25.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Regular;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 50.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Greater;
+    		    else if (roll < 0.60) chosen = Poison.Regular;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 75.0)
+    		{
+    		    if (roll < 0.20) chosen = Poison.Deadly;
+    		    else if (roll < 0.40) chosen = Poison.Greater;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else if (poisoning < 100.0)
+    		{
+    		    if (roll < 0.33) chosen = Poison.Deadly;
+    		    else if (roll < 0.77) chosen = Poison.Greater;
+    		    else chosen = Poison.Lesser;
+    		}
+    		else
+    		{
+    		    if (roll < 0.44) chosen = Poison.Deadly;
+    		    else chosen = Poison.Greater;
+    		}
+
+    		if (chosen != null)
+    		{
+    		    defender.ApplyPoison(attacker, chosen);
+
+    		    if (chosen == Poison.Lesser)
+    		        Misc.Titles.AwardKarma(attacker, -50, true);
+    		    else if (chosen == Poison.Regular)
+    		        Misc.Titles.AwardKarma(attacker, -60, true);
+    		    else if (chosen == Poison.Greater)
+    		        Misc.Titles.AwardKarma(attacker, -70, true);
+    		    else if (chosen == Poison.Deadly)
+    		        Misc.Titles.AwardKarma(attacker, -80, true);
+    		}
 		}
 
 		public override void GetDamageTypes( Mobile wielder, out int phys, out int fire, out int cold, out int pois, out int nrgy, out int chaos, out int direct )
