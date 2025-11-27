8341e8d555ce1e29f3d06b237750fedb97eaa731
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-27 15:37:31 -0300

    grim reaper's scythe now heals when applying culling strikes

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_GrimReapersScythe.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_GrimReapersScythe.cs
@@ -16,13 +16,35 @@ namespace Server.Items
 			Hue = 0x47E;
 			Name = "Grim Reaper's Scythe";
 			ItemID = 0x2690;
-			WeaponAttributes.HitLeechHits = 30;
-			WeaponAttributes.UseBestSkill = 1;
+			WeaponAttributes.HitLeechHits = 50;
+			Attributes.SpellChanneling = 1;
 			AccuracyLevel = WeaponAccuracyLevel.Supremely;
  	 	    Slayer = SlayerName.Repond;
 			ArtifactLevel = 2;
-			Server.Misc.Arty.ArtySetup( this, "" );
+			Server.Misc.Arty.ArtySetup( this, "Reaps souls." );
 		}
+
+		public override void OnHit(Mobile attacker, Mobile defender, double damageBonus)
+        {
+            if (attacker == null || defender == null)
+                return;
+
+            if (defender.Hits > 0 && defender.Hits < (defender.HitsMax / 10))
+            {
+                int extra = (int)(defender.HitsMax * 0.25);
+                if (extra < 1)
+                    extra = 1;
+				else if (extra > 25)
+					extra = 25;
+
+                attacker.Heal(extra, attacker);
+
+                attacker.FixedParticles(0x3728, 10, 10, 5052, 0, 0, EffectLayer.Head);
+                attacker.PlaySound(0x1F1);
+            }
+
+            base.OnHit(attacker, defender, damageBonus);
+        }
 
 		public Artifact_GrimReapersScythe( Serial serial ) : base( serial )
 		{
