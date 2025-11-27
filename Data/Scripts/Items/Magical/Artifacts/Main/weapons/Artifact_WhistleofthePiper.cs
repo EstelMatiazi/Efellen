b6d8544eb3fcc81a54cd3df0e7433110f4cccef8
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-27 16:23:00 -0300

    adds root effect to whistle of the piper

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_WhistleofthePiper.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_WhistleofthePiper.cs
@@ -14,14 +14,37 @@ namespace Server.Items
 		{
 			Name = "Whistle of the Pied Piper";
 			Hue = 0x668;
-			SkillBonuses.SetValues( 0, SkillName.Taming,  10.0);
-			SkillBonuses.SetValues( 1, SkillName.Herding,  10.0);
-			WeaponAttributes.HitLeechStam = 30;
-			WeaponAttributes.HitLeechHits = 30;
+			SkillBonuses.SetValues( 1, SkillName.Herding,  20.0);
+			WeaponAttributes.HitLeechStam = 40;
             Attributes.WeaponSpeed = 10;
 			ArtifactLevel = 2;
-			Server.Misc.Arty.ArtySetup( this, "" );
+			Server.Misc.Arty.ArtySetup( this, "Immobilizes animals" );
 		}
+
+		public override void OnHit(Mobile attacker, Mobile defender, double damageBonus)
+        {
+            base.OnHit(attacker, defender, damageBonus);
+
+            if (DateTime.Now < m_NextParalyze)
+                return;
+
+            bool validTarget = false;
+            SlayerEntry animal = SlayerGroup.GetEntryByName(SlayerName.AnimalHunter);
+			if (animal != null && animal.Slays(defender))
+                validTarget = true;
+			if (!validTarget)
+                return;
+
+            if (Utility.RandomDouble() < 0.25)
+            {
+                if (defender != null && defender.Alive && !defender.Paralyzed)
+                {
+                    defender.Paralyze(TimeSpan.FromSeconds(5));
+                    attacker.SendMessage("Your blow immobilizes your foe!");
+                    m_NextParalyze = DateTime.Now + TimeSpan.FromSeconds(30);
+                }
+            }
+        }
 
 		public override void GetDamageTypes( Mobile wielder, out int phys, out int fire, out int cold, out int pois, out int nrgy, out int chaos, out int direct )
 		{
