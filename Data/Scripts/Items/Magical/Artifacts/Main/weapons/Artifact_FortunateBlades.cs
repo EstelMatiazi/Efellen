fa8aa0d02366a51297703eb20a0ee0f07285349d
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-27 21:32:49 -0300

    balance pass on fortunate blades

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_FortunateBlades.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_FortunateBlades.cs
@@ -17,11 +17,8 @@ namespace Server.Items
 			Hue = 2213;
 			WeaponAttributes.MageWeapon = 30;
 			Attributes.SpellChanneling = 1;
-			Attributes.CastSpeed = 1;
-			WeaponAttributes.SelfRepair = 5;
-			Attributes.Luck = 45;
-			Attributes.RegenMana = 5;
-			Attributes.SpellDamage = 15;
+			Attributes.Luck = 85;
+			Attributes.SpellDamage = 20;
 			ArtifactLevel = 2;
 			Server.Misc.Arty.ArtySetup( this, "" );
 		}
