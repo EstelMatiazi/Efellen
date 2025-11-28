07ded70ef65e53818e6f9019ef1c3888e7425716
EstelMatiazi <estelmatiazi@gmail.com>
2025-11-28 09:01:36 -0300

    adds random dot to jade scimitar

--- Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_JadeScimitar.cs
+++ Data/Scripts/Items/Magical/Artifacts/Main/weapons/Artifact_JadeScimitar.cs
@@ -2,11 +2,13 @@ using System;
 using Server.Network;
 using Server.Items;
 using Server.Targeting;
+using Server.CustomEffects;
 
 namespace Server.Items
 {
 	public class Artifact_JadeScimitar : GiftScimitar
 	{
+		private DateTime m_NextArtifactAttackAllowed;
 		public override int InitMinHits{ get{ return 80; } }
 		public override int InitMaxHits{ get{ return 160; } }
 
@@ -23,6 +25,35 @@ namespace Server.Items
 			WeaponAttributes.HitPoisonArea = 20;
 			ArtifactLevel = 2;
 			Server.Misc.Arty.ArtySetup( this, "" );
+			m_NextArtifactAttackAllowed = DateTime.MinValue;
+		}
+
+		public override void OnHit(Mobile attacker, Mobile defender, double damageBonus)
+		{
+			base.OnHit(attacker, defender, damageBonus);
+
+			if (attacker == null || defender == null || defender.Deleted)
+				return;
+
+			if (DateTime.UtcNow < m_NextArtifactAttackAllowed)
+				return;
+
+			if (Utility.RandomDouble() > 0.15)
+				return;
+
+			double skill = attacker.Skills[SkillName.Swordsmanship].Value;
+			int duration = 4 + (int)(skill / 25.0);
+
+			if (duration < 4) duration = 4;
+			if (duration > 9) duration = 9;
+
+
+			DotEffect.ApplyDot(defender, duration, attacker, Utility.RandomMinMax(1, 5));
+
+			attacker.SendMessage(33, "The Jade Scimitar bursts with power!");
+			attacker.PlaySound(0x208);
+
+			m_NextArtifactAttackAllowed = DateTime.UtcNow + TimeSpan.FromMinutes(2);
 		}
 
 		public Artifact_JadeScimitar( Serial serial ) : base( serial )
@@ -32,14 +63,20 @@ namespace Server.Items
 		public override void Serialize( GenericWriter writer )
 		{
 			base.Serialize( writer );
-			writer.Write( (int) 0 ); // version
+
+			writer.WriteEncodedInt( 0 ); // version
+			writer.Write(m_NextArtifactAttackAllowed);
 		}
 
-		public override void Deserialize( GenericReader reader )
+		public override void Deserialize(GenericReader reader)
 		{
-			base.Deserialize( reader );
-			ArtifactLevel = 2;
+			base.Deserialize(reader);
 			int version = reader.ReadInt();
+
+			if (version >= 0)
+				m_NextArtifactAttackAllowed = reader.ReadDateTime();
+
+			ArtifactLevel = 2;
 		}
 	}
 }
